a208a8f384417e3d3f68fd9d58121347b674e7e7
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-03-05 16:09:55 +0100

    add lowres experiments

---
+++ nati_experiments/lowreswgan64.py
@@ -0,0 +1,110 @@
+
+# coding: utf-8
+
+import os, sys
+import pickle
+
+import sys
+sys.path.insert(0, '../')
+
+import data
+import numpy as np
+from model import WGanModel, LapGanModel
+from gan import CosmoGAN
+import utils
+
+# Parameters
+
+ns = 64
+nsamples = 15000
+k = 10
+try_resume = False
+scaling = [4]
+
+
+
+
+time_str = 'quarter_img'
+global_path = '../../../saved_result/'
+
+latent_dim = 100
+bn = False
+params_discriminator = dict()
+params_discriminator['stride'] = [2, 2, 2, 1, 1]
+params_discriminator['nfilter'] = [16, 64, 128, 256, 64]
+params_discriminator['shape'] = [[5, 5], [5, 5], [5, 5], [3, 3], [3, 3]]
+params_discriminator['batch_norm'] = [bn, bn, bn, bn, bn]
+params_discriminator['full'] = [32]
+params_discriminator['summary'] = True
+params_discriminator['minibatch_reg'] = False
+
+params_generator = dict()
+params_generator['stride'] = [2, 2, 2, 1, 1, 1]
+params_generator['latent_dim'] = latent_dim
+params_generator['nfilter'] = [16, 64, 128, 256, 64, 1]
+params_generator['shape'] = [[3, 3], [5, 5], [5, 5], [5, 5], [5, 5], [5, 5]]
+params_generator['batch_norm'] = [bn, bn, bn, bn, bn]
+params_generator['full'] = [8*8*16]
+params_generator['summary'] = True
+params_generator['non_lin'] = None
+
+
+params_optimization = dict()
+params_optimization['gamma_gp'] = 10
+params_optimization['batch_size'] = 16
+params_optimization['gen_optimizer'] = 'rmsprop' # rmsprop / adam / sgd
+params_optimization['disc_optimizer'] = 'rmsprop' # rmsprop / adam /sgd
+params_optimization['disc_learning_rate'] = 3e-5
+params_optimization['gen_learning_rate'] = 3e-5
+params_optimization['beta1'] = 0.9
+params_optimization['beta2'] = 0.999
+params_optimization['epsilon'] = 1e-8
+params_optimization['epoch'] = 50
+
+params_cosmology = dict()
+params_cosmology['clip_max_real'] = False
+params_cosmology['log_clip'] = 0.1
+params_cosmology['sigma_smooth'] = 1
+params_cosmology['k'] = k
+params_cosmology['Npsd'] = 500
+
+params = dict()
+params['generator'] = params_generator
+params['discriminator'] = params_discriminator
+params['optimization'] = params_optimization
+params['cosmology'] = params_cosmology
+
+params['normalize'] = False
+params['image_size'] = [ns, ns]
+params['prior_distribution'] = 'gaussian'
+params['sum_every'] = 200
+params['viz_every'] = 200
+params['save_every'] = 5000
+params['name'] = 'LowRes{}_'.format(ns)
+params['summary_dir'] = global_path + params['name'] + '_' + time_str +'_summary/'
+params['save_dir'] = global_path + params['name'] + '_' + time_str + '_checkpoints/'
+
+
+
+
+
+resume, params = utils.test_resume(try_resume, params)
+
+
+# Build the model
+
+wgan = CosmoGAN(params, WGanModel)
+
+images, raw_images = data.load_samples(nsamples = nsamples, permute=True, k=k)
+images = data.make_smaller_samples(images, 256)
+raw_images = data.make_smaller_samples(raw_images, 256)   
+down_sampled_images = data.down_sample_images(images, scaling)
+
+
+# Train the model
+wgan.train(down_sampled_images[1], resume=resume)
+
+
+
+
+
