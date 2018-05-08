38b4e6bc6e2d4fefa802fed291b423bbc3a04056
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-05-08 11:34:51 +0200

    update default parameters

--- default.py
+++ default.py
@@ -2,75 +2,64 @@ import data.fmap as fmap
 import numpy as np
 import warnings
 
-# def arg_helper(params, keys, value):
-#     if not (type(keys) is list):
-#         keys = [keys]
+def arg_helper(params, d_param):
+    for key in d_param.keys():
+        params[key] = params.get(key, d_param[key])
+        if type(params[key]) is dict:
+            params[key] = arg_helper(params[key], d_param[key])
+    return params
 
-#     for level, key in enumerate(keys):
-#         if not (key in params.keys()):
-#             if 
-#             params[key] = 
-
-#     return params
 
 def default_params(params=dict()):
 
     # Global parameters
     # -----------------
-    params['print_every'] = params.get('print_every', 100)
+    d_param = dict()
+    d_param['print_every'] = 100
     # Print the losses in the consol every 'print_every' iterations
-    params['save_every'] = params.get('save_every', 100)
+    d_param['save_every'] = 2000
     # Save the model every 'save_every' iterations
-    params['sum_every'] = params.get('sum_every', 100)
+    d_param['sum_every'] = 200
     # Compute the numerical summaries every 'sum_every' iterations
-    params['viz_every'] = params.get('viz_every', 100)
+    d_param['viz_every'] = 200
     # Buil the visual summaries every 'viz_every' iterations
-    params['normalize'] = params.get('normalize', False)
+    d_param['normalize'] = False
     # Apply a normalization step to the data
-    params['resume'] = params.get('resume', False)
+    d_param['resume'] = False
     # Resume training. Warning, needs exactly the same parameters
-    params['prior_distribution'] = params.get('prior_distribution',
-                                              'gaussian')
+    d_param['prior_distribution'] = 'gaussian'
     # Prior distribution to sample from ('Gaussian','Uniform',...)
-    params['image_size'] = params.get('image_size', [32, 32, 1])
+    d_param['image_size'] = [32, 32, 1]
     # size of input image
-
 
     # Discriminator parameters
     # ------------------------
-    params['discriminator'] = params.get('discriminator', dict())
-    params['discriminator']['minibatch_reg'] = params['discriminator'].get(
-        'minibatch_reg', False)
+    d_param['discriminator'] = dict()
+    d_param['discriminator']['minibatch_reg'] = False
     # Minibatch regularization
-    # params['discriminator']['minibatch_reg'] = False
+    # d_param['discriminator']['minibatch_reg'] = False
     # print('Minibatch regularization set to False (Force)')
-    params['discriminator']['non_lin'] = params['discriminator'].get(
-        'non_lin', None)
-    params['discriminator']['one_pixel_mapping'] = params['discriminator'].get('one_pixel_mapping', [])
+    d_param['discriminator']['non_lin'] =  None
+    d_param['discriminator']['one_pixel_mapping'] = []
 
     # Optimization parameters
     # -----------------------
-    params['optimization'] = params.get('optimization', dict())
-    params['optimization']['disc_optimizer'] = params['optimization'].get(
-        'disc_optimizer', "adam")
-    params['optimization']['gen_optimizer'] = params['optimization'].get(
-        'gen_optimizer', "adam")
-    params['optimization']['gen_learning_rate'] = params['optimization'].get(
-        'gen_learning_rate', 3e-5)
-    params['optimization']['enc_learning_rate'] = params['optimization'].get(
-        'enc_learning_rate', 3e-5)
-    params['optimization']['disc_learning_rate'] = params['optimization'].get(
-        'disc_learning_rate', 3e-5)
-    params['optimization']['n_critic'] = params['optimization'].get(
-        'n_critic', 5)
+    d_param['optimization'] = dict()
+    d_param['optimization']['disc_optimizer'] = "rmsprop"
+    d_param['optimization']['gen_optimizer'] = "rmsprop"
+    d_param['optimization']['enc_optimizer'] = "rmsprop"
+    d_param['optimization']['gen_learning_rate'] = 3e-5
+    d_param['optimization']['enc_learning_rate'] = 3e-5
+    d_param['optimization']['disc_learning_rate'] = 3e-5
+    d_param['optimization']['n_critic'] = 5
 
     # Generator parameters
     # --------------------
-    params['generator'] = params.get('generator', dict())
-    params['generator']['y_layer'] = params['generator'].get('y_layer', None)
-    params['generator']['one_pixel_mapping'] = params['generator'].get('one_pixel_mapping', [])
+    d_param['generator'] = dict()
+    d_param['generator']['y_layer'] = None
+    d_param['generator']['one_pixel_mapping'] = []
 
-    return params
+    return arg_helper(params, d_param)
 
 
 def default_params_cosmology(params=dict()):
@@ -78,24 +67,24 @@ def default_params_cosmology(params=dict()):
     forward = fmap.forward
     backward = fmap.backward 
 
-    params = default_params(params)
+    d_param = default_params()
     # Cosmology parameters
     # --------------------
-    params['cosmology'] = params.get('cosmology', dict())
+    d_param['cosmology'] = dict()
+    d_param['cosmology']['Nstats'] = 500
+    d_param['cosmology']['forward_map'] = forward
+    d_param['cosmology']['backward_map'] = backward
+    # Default transformation for the data
 
     if 'Npsd' in params['cosmology'].keys():
         params['cosmology']['Nstats'] = params['cosmology']['Npsd']
         warnings.warn('Use Nstats instead of Npsd!')
-    params['cosmology']['Nstats'] = params['cosmology'].get('Nstats', 500)
 
-    params['cosmology']['forward_map'] = params['cosmology'].get('forward_map', forward)
-    params['cosmology']['backward_map'] = params['cosmology'].get('backward_map', backward)
-    # Default transformation for the data
-
-    return params
+    return arg_helper(params, d_param)
 
 
 def default_params_time(params=dict()):
+    warnings.warn('TODO update this code to be ressemble the rest...')
     params['time']['num_classes'] = params['time'].get('num_classes', 1)
     # Number of classes to condition on
     params['time']['classes'] = params['time'].get('classes', None)
