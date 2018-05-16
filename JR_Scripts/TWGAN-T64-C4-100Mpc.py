0b262db2a2e9c0ea4435e3d5b2eeb0079ea7c2ed
Jonathan <rosenthj@ethz.ch>
2018-05-16 17:49:54 +0200

    500Mpc testing larger generator architecture

--- JR_Scripts/TWGAN-T64-C4-100Mpc.py
+++ JR_Scripts/TWGAN-T64-C4-100Mpc.py
@@ -34,7 +34,7 @@ forward = functools.partial(fmap.stat_forward, shift=shift, c=bandwidth)
 backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 
 
-time_str = '0r-2r_7v6_{}'.format(Mpch)
+time_str = '0r-2r_7v6s_{}'.format(Mpch)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
 name = 'TWGAN{}_SF_'.format(ns)
@@ -83,7 +83,7 @@ params_cosmology['Nstats'] = 1000
 params_time = dict()
 params_time['num_classes'] = 2
 params_time['classes'] = [2, 0]
-params_time['class_weights'] = [0.84, 1]
+params_time['class_weights'] = [0.9, 1.11]
 params_time['model_idx'] = 2
 
 params_optimization['batch_size_gen'] = params_optimization['batch_size'] * params_time['num_classes']
