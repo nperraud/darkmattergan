19b9910a1580013d8bd7b97e4bc8c0095d23c784
Jonathan <rosenthj@ethz.ch>
2018-05-16 11:28:15 +0200

    Testing a higher learning rate

--- JR_Scripts/TWGAN-T64-C4-100Mpc.py
+++ JR_Scripts/TWGAN-T64-C4-100Mpc.py
@@ -37,7 +37,7 @@ backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 time_str = '0r_{}'.format(Mpch)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
-name = 'TWGAN{}_SF_'.format(ns)
+name = 'TWGAN{}_LR4e5'.format(ns)
 
 bn = False
 
@@ -65,8 +65,8 @@ params_optimization['gamma_gp'] = 10
 params_optimization['batch_size'] = 16
 params_optimization['gen_optimizer'] = 'adam' # rmsprop / adam / sgd
 params_optimization['disc_optimizer'] = 'adam' # rmsprop / adam /sgd
-params_optimization['disc_learning_rate'] = 1e-5
-params_optimization['gen_learning_rate'] = 1e-5
+params_optimization['disc_learning_rate'] = 4e-5
+params_optimization['gen_learning_rate'] = 4e-5
 params_optimization['beta1'] = 0.5
 params_optimization['beta2'] = 0.99
 params_optimization['epsilon'] = 1e-8
