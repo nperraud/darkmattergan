677f149344daa71e6f662b0f99f1589c0fd373d8
Jonathan <rosenthj@ethz.ch>
2018-05-29 14:15:33 +0200

    Testing with just CDF

--- JR_Scripts/TWGAN-T32-C2.py
+++ JR_Scripts/TWGAN-T32-C2.py
@@ -40,7 +40,7 @@ backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 time_str = '0r-2r_0911{}'.format(Mpch)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
-name = 'TWGAN{}|6-5_aMom+CDF'.format(ns)
+name = 'TWGAN{}|6-5_CDF'.format(ns)
 
 bn = False
 
@@ -51,7 +51,7 @@ params_discriminator['shape'] = [[5, 5],[5, 5],[5, 5], [3, 3], [3, 3]]
 params_discriminator['batch_norm'] = [bn] * len(params_discriminator['nfilter'])
 params_discriminator['full'] = [64]
 params_discriminator['cdf'] = 256
-params_discriminator['moment'] = [5,5]
+#params_discriminator['moment'] = [5,5]
 params_discriminator['minibatch_reg'] = False
 params_discriminator['summary'] = True
 
