BUG: Fixed wasserstein weighting in total stats error.
Previously this was almost certainly done in an unintentional way. This new iteration makes more sense, however I am not certain if we should be using log10?

metrics.py
@@ -345,7 +345,7 @@ def total_stats_error(feed_dict, params=dict()):
     v += params.get("w_l1_log_mass_hist", 0) * feed_dict['log_l1_mass_hist']
     v += params.get("w_l2_log_peak_hist", 1) * feed_dict['log_l2_peak_hist']
     v += params.get("w_l1_log_peak_hist", 0) * feed_dict['log_l1_peak_hist']
-    v += np.log10(params.get("w_wasserstein_mass_hist", 0)+1)\
-         * feed_dict['wasserstein_mass_hist']
+    v += params.get("w_wasserstein_mass_hist", 0)\
+         * np.log10(feed_dict['wasserstein_mass_hist'] + 1)
 
     return v