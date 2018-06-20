bd390239c28a406ada0ed6c025378f536f2dac14
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-06-20 16:07:25 +0200

    Testing v5 with small CDF block

--- JR_Scripts/TWGAN-T32-C2.py
+++ JR_Scripts/TWGAN-T32-C2.py
@@ -38,7 +38,7 @@ bandwidth = 20000
 forward = functools.partial(fmap.stat_forward, shift=shift, c=bandwidth)
 backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 
-time_str = '0r-24-6r_0811_chCDF-Mom{}'.format(Mpch)
+time_str = '0r-24-6r_0811_16x8chCDF-Mom{}'.format(Mpch)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
 name = 'TWGANv5:v2{}_6-5_'.format(ns)
@@ -51,9 +51,9 @@ params_discriminator['nfilter'] = [16, 128, 256, 128, 64]
 params_discriminator['shape'] = [[5, 5],[5, 5],[5, 5], [3, 3], [3, 3]]
 params_discriminator['batch_norm'] = [bn] * len(params_discriminator['nfilter'])
 params_discriminator['full'] = [64]
-params_discriminator['cdf'] = 256
-params_discriminator['channel_cdf'] = 128
-params_discriminator['moment'] = [5,5]
+params_discriminator['cdf'] = 16
+params_discriminator['channel_cdf'] = 8
+#params_discriminator['moment'] = [5,5]
 params_discriminator['minibatch_reg'] = False
 params_discriminator['summary'] = True
 
