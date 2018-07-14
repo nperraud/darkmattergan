98c2c50be79aedc74bd8e1cae7db52360725c311
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-07-14 22:09:04 +0200

    Turning Momment block on

--- JR_Scripts/TWGAN-C1.py
+++ JR_Scripts/TWGAN-C1.py
@@ -46,7 +46,7 @@ backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 time_str = '{}r_CDF{}'.format(cl, Mpc)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
-name = 'TWGANv{}:{}d{}_sn6-5_4'.format(model_idx, Mpc, divisor)
+name = 'TWGANv{}:{}d{}_sn6-5_4Mom'.format(model_idx, Mpc, divisor)
 
 bnd = False
 
@@ -58,7 +58,7 @@ params_discriminator['batch_norm'] = [bnd] * len(params_discriminator['nfilter']
 params_discriminator['full'] = [64]
 params_discriminator['cdf'] = 16
 #params_discriminator['channel_cdf'] = 8
-#params_discriminator['moment'] = [5,5]
+params_discriminator['moment'] = [5,5]
 params_discriminator['minibatch_reg'] = False
 params_discriminator['summary'] = True
 
