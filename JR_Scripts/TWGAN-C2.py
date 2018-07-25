7c1065c2bc430b8c42f151584b9323f1e02328cb
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-07-25 17:45:56 +0200

    Testing learned histogram block

--- JR_Scripts/TWGAN-C2.py
+++ JR_Scripts/TWGAN-C2.py
@@ -62,7 +62,7 @@ forward = functools.partial(fmap.stat_forward, shift=shift, c=bandwidth)
 backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 
 #time_str = '0r-24-6r_0811_16x8chCDF-Mom{}'.format(Mpch)
-time_str = '{}{}r_js-reg_c+M{}'.format(cl[0], cl[1], Mpc)
+time_str = '{}{}r_lhist_c+M{}'.format(cl[0], cl[1], Mpc)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
 bnd = False
