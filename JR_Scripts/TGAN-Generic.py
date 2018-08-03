b410a68870bddd5a5cc20a4ccdd1dfb435fb97d7
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-08-03 17:38:24 +0200

    Fixing a bug

--- JR_Scripts/TGAN-Generic.py
+++ JR_Scripts/TGAN-Generic.py
@@ -31,7 +31,7 @@ def save_dict(params):
 def get_class_weights(cls):
     weights = []
     for i in range(len(cls)):
-        weights.append(1.3 - (0.08*cl[0]))
+        weights.append(1.3 - (0.08*cls[i]))
     return weights
 
 # Parameters
