7e026a67294fb9f0c79ce618ef3810826c955b3d
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-08-05 18:56:59 +0200

    Removing moment block, input separation for discriminator

--- JR_Scripts/TGAN-Generic.py
+++ JR_Scripts/TGAN-Generic.py
@@ -74,7 +74,7 @@ backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 cl_str = ''
 for cl_id in cl:
     cl_str = cl_str + str(cl_id)
-time_str = '{}r_lr1e4relv2ad4_b5_c+M{}'.format(cl_str, Mpc)
+time_str = '{}r_lr1e4relv2ad4_b5_c+sf{}'.format(cl_str, Mpc)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
 bnd = False
@@ -90,9 +90,10 @@ discriminator_net.add_full(64)
 
 params_discriminator = discriminator_net.params
 params_discriminator['spectral_norm'] = True
+params_discriminator['separate_first'] = True
 # params_discriminator['cdf'] = 32
 # params_discriminator['channel_cdf'] = 16
-params_discriminator['moment'] = [5,5]
+# params_discriminator['moment'] = [5,5]
 params_discriminator['minibatch_reg'] = False
 params_discriminator['summary'] = True
 params_cdf = dict()
