069dbed60b047b22669171da4b5d292069ce5226
Jonathan Rosenthal <rosenthj@ethz.ch>
2018-08-05 20:47:36 +0200

    Testing other network architecture and rms prop

--- JR_Scripts/TGAN-Generic.py
+++ JR_Scripts/TGAN-Generic.py
@@ -74,16 +74,13 @@ backward = functools.partial(fmap.stat_backward, shift=shift, c=bandwidth)
 cl_str = ''
 for cl_id in cl:
     cl_str = cl_str + str(cl_id)
-time_str = '{}r_lr3e5relv2rms4d_b5_c+sf{}'.format(cl_str, Mpc)
+time_str = '{}r_lr3e5relv2rms4_b5_c+sf{}'.format(cl_str, Mpc)
 global_path = '/scratch/snx3000/rosenthj/results/'
 
 bnd = False
 
 discriminator_net = utils.NetParamHelper()
-discriminator_net.add_conv_layer(128, stride=1, shape=5)
-discriminator_net.add_conv_layer(16, stride=1, shape=1)
-discriminator_net.add_conv_layer(64, stride=2, shape=5)
-discriminator_net.add_conv_layer(64, stride=2, shape=5)
+discriminator_net.add_conv_layer(96, stride=2, shape=5)
 discriminator_net.add_conv_layer(128, stride=2, shape=5)
 discriminator_net.add_conv_layer(256, stride=2, shape=5)
 discriminator_net.add_conv_layer(256, stride=2, shape=5)
