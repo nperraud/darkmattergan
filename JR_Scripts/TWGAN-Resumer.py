Fixed resumer data scaling

JR_Scripts/TWGAN-Resumer.py
@@ -25,6 +25,7 @@ ns = 64
 try_resume = True
 Mpc_orig = 500
 Mpc = Mpc_orig // (512 // ns)
+divisor = 3
 
 params = load_gan_params(sys.argv[1])
 utils.print_param_dict(params)
@@ -43,7 +44,7 @@ if resume:
     filename = '/scratch/snx3000/rosenthj/data/nbody_{}Mpc_All.h5'.format(Mpc_orig)
     for box_idx in params['time']['classes']:
         images = utils.load_hdf5(filename=filename, dataset_name=str(box_idx), mode='r')
-        images = params['cosmology']['forward_map'](images)
+        images = params['cosmology']['forward_map'](images / divisor)
         #while images.shape[1] > ns:
         #    images = skimage.measure.block_reduce(images, (1,2,2), np.sum)
         img_list.append(images)