31f202be4d738b018ebc6fb403f730e717a4bf81
Nathanaël Perraudin <nperraud@users.noreply.github.com>
2018-10-04 14:08:07 +0200

    add notebook for evaluation of nsynth

--- gantools/data/path.py
+++ gantools/data/path.py
@@ -50,6 +50,8 @@ def nsynth_path():
     hostname = socket.gethostname()
     if ('nid' in hostname) or ('daint' in hostname):
         rootpath = '/scratch/snx1600/nperraud/data/'
+    elif 'omenx' in hostname:
+        rootpath = '/store/nati/dataset/Nsynth-gan/'
     else:
         utils_module_path = os.path.dirname(__file__)
 
