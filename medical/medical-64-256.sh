31111e40923eb0d7127908b3ed3c8a5e0f98b272
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-10-22 10:28:19 +0200

    launcher script

---
+++ medical/medical-64-256.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --constraint=gpu
+#SBATCH --output=medical-256-%j.log
+#SBATCH --error=medical-256-e-%j.log
+
+module load daint-gpu
+module load cray-python
+module load TensorFlow/1.7.0-CrayGNU-18.08-cuda-9.1-python3
+
+source $HOME/upgan/bin/activate
+
+cd $SCRATCH/CodeGAN/medical/
+srun python medical-64-256.py
---
+++ nbody-3D/nbody-0-32.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --constraint=gpu
+#SBATCH --output=nbody-32-%j.log
+#SBATCH --error=nbody-32-e-%j.log
+
+module load daint-gpu
+module load cray-python
+module load TensorFlow/1.7.0-CrayGNU-18.08-cuda-9.1-python3
+
+source $HOME/upgan/bin/activate
+
+cd $SCRATCH/CodeGAN/nbody-3D/
+srun python nbody-0-32.py
