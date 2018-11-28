4f5e736f544c8d5616146010d513c0516a9a57ba
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-11-28 12:20:27 +0100

    make a working version of 32

--- nbody-3D/nbody-ankit-32.py
+++ nbody-3D/nbody-ankit-32.py
@@ -14,47 +14,43 @@ time_str = '0_to_32'
 global_path = '../saved_results/nbody/'
 name = 'WGAN_ankit_' + time_str
 
-params_discriminator: {
-	'batch_norm': [False, False, False, False, False, False],
-	'full': [64, 16],
-	'inception': True,
-	'is_3d': True,
-	'minibatch_reg': False,
-	'nfilter': [64, 64, 32, 16, 8, 2],
-	'non_lin': None,
-	'one_pixel_mapping': [],
-	'stride': [2, 2, 1, 1, 1, 1],
-	'summary': True}
+params_discriminator = {
+    'batch_norm': [False, False, False, False, False, False],
+    'data_size': 3,
+    'full': [64, 16],
+    'inception': True,
+    'minibatch_reg': False,
+    'nfilter': [64, 64, 32, 16, 8, 2],
+    'non_lin': None,
+    'one_pixel_mapping': [],
+    'stride': [2, 2, 1, 1, 1, 1],
+    'summary': True}
 
-params_generator = {
-	'batch_norm': [False, False, False, False, False, False, False],
-	'downsampling': None,
-	'full': [64],
-	'inception': True,
-	'is_3d': True,
-	'latent_dim': 100,
-	'nfilter': [8, 32, 64, 64, 64, 32, 32, 1],
-	'non_lin': tf.nn.relu,
-	'one_pixel_mapping': [],
-	'residual': False,
-	'stride': [2, 2, 2, 2, 1, 1, 1, 1],
-	'summary': True,
-	'y_layer': None}
+params_generator = {'batch_norm': [False,
+    False,
+    False,
+    False,
+    False,
+    False,
+    False],
+   'data_size': 3,
+   'full': [64],
+   'inception': True,
+   'latent_dim': 100,
+   'nfilter': [8, 32, 64, 64, 64, 32, 32, 1],
+   'non_lin': tf.nn.relu,
+   'one_pixel_mapping': [],
+   'residual': False,
+   'stride': [2, 2, 2, 2, 1, 1, 1, 1],
+   'summary': True}
 
- params_optimization =  {
- 	'batch_size': 8,
-	'beta1': 0.9,
-	'beta2': 0.999,
-	'disc_learning_rate': 3e-05,
-	'disc_optimizer': 'rmsprop',
-	'enc_learning_rate': 3e-05,
-	'enc_optimizer': 'rmsprop',
-	'epoch': 10000,
-	'epsilon': 1e-08,
-	'gamma_gp': 5,
-	'gen_learning_rate': 3e-05,
-	'gen_optimizer': 'rmsprop',
-	'n_critic': 10},
+params_optimization =  {
+    'batch_size': 8,
+    'discriminator': {'kwargs': {}, 'learning_rate': 3e-05, 'optimizer': 'rmsprop'},
+    'encoder': {'kwargs': {}, 'learning_rate': 3e-05, 'optimizer': 'rmsprop'},
+    'epoch': 10000,
+    'generator': {'kwargs': {}, 'learning_rate': 3e-05, 'optimizer': 'rmsprop'},
+    'n_critic': 10}
 
 
 params_cosmology = dict()
@@ -65,9 +61,10 @@ params = dict()
 params['net'] = dict()
 params['net']['shape'] = [ns, ns, ns, 1]
 params['net']['generator'] = params_generator
-params['net']['gamma'] = 10
+params['net']['gamma'] = 5
 params['net']['discriminator'] = params_discriminator
 params['net']['cosmology'] = params_cosmology
+params['net']['prior_distribution'] = 'gaussian'
 
 params['optimization'] = params_optimization
 params['summary_every'] = 100  # Tensorboard summaries every ** iterations
