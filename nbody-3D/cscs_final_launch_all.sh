34615068428c6e88ac4fb4406c0c3b9bc991c6d8
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-12-20 17:43:53 +0100

    add final experiment

---
+++ nbody-3D/cscs_final_launch_all.sh
@@ -0,0 +1,4 @@
+sbatch nbody-final-0-32.sh
+sbatch nbody-final-32-64.sh
+sbatch nbody-final-64-256.sh
+sbatch nbody-final-uniscale.sh
---
+++ nbody-3D/nbody-final-64-256.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --account=sd01
+#SBATCH --constraint=gpu
+#SBATCH --output=nbody-final-256-%j.log
+#SBATCH --error=nbody-final-256-e-%j.log
+
+module load daint-gpu
+module load cray-python
+module load TensorFlow/1.7.0-CrayGNU-18.08-cuda-9.1-python3
+
+source $HOME/upgan/bin/activate
+
+cd $SCRATCH/CodeGAN/nbody-3D/
+srun python nbody-final-64-256.py
