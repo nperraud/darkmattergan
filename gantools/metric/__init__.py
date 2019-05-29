58abbf182ec04c1a56c6851303905b95f0209ace
Nathanael Perraudin <nperraud@users.noreply.github.com>
2019-05-29 14:25:00 +0200

    remove gantools

--- gantools/metric/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-r"""
-The metric module contains all the function to compute the metrics for the generated images
-"""
-
-from .core import *
-from . import ganlist
-from . import stats
