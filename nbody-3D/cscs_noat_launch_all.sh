df2d73c53629c7b02675d214712f75c1c033800f
Nathanaël Perraudin <nperraud@users.noreply.github.com>
2019-06-26 13:13:23 +0200

    remove unecessary experiments

--- nbody-3D/cscs_noat_launch_all.sh
+++
@@ -1,3 +0,0 @@
-sbatch nbody-noat-32-64.sh
-sbatch nbody-noat-64-256.sh
-sbatch nbody-noat-uniscale.sh
--- nbody-3D/cscs_symmetric_launch_all.sh
+++
@@ -1,3 +0,0 @@
-sbatch nbody-symmetric-border-32-64.sh
-sbatch nbody-symmetric-border-64-256.sh
-sbatch nbody-symmetric-border-uniscale.sh
